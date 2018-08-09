dec3dd3eba5daf44a8a9ad617626eec9fa57b514
julius bade <julbade21@gmail.com>
2018-08-09 00:11:50 -0700

    updated styles and page

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,14 @@ $(document).ready(function() {
   var nameInput = $("input#name").val();
   var addressInput = $("input#address").val();
   var shoesInput = $("input#shoes").val();
+  var sizeInput = $("input#size").val();
 
 
 
     $(".name").text(nameInput);
     $(".address").text(addressInput);
     $(".shoes").text(shoesInput);
+    $(".size").text(sizeInput);
     $(".shoedetails").toggle();
     $("#receipt").toggle();
 
